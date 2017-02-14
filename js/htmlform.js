b5399e475844735154670bff8b676ec5541976c1
Unknown <ali@faradji.se>
2017-02-14 18:42:26 +0100

    förberrede elev sidan med checkbox val

---
+++ js/htmlform.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	
+	$('.option').append('<p>fr&aring;gan h&auml;r</p><br/>');
+	$('.option').append('<input type="checkbox" name="svar-ett" /><br/><br/>');
+	$('.option').append('<input type="checkbox" name="svar-tva" /><br/>');
+	$('.questions').append('<input type="button" value="n&auml;sta fr&aring;ga"/>');
+	
+});
+
