aeb406af5eed566ebfb3c9adc96f1b2ab486ddda
Dijana <dockan_7@hotmail.com>
2017-02-27 00:27:55 +0100

    Skiftade från endast .js filer från mejlView och LärarView till html templates med js funktioner

--- med nya skelettet/js/insertDoneTestsListInTeachView.js
+++ med nya skelettet/js/insertDoneTestsListInTeachView.js
@@ -1,71 +1,17 @@
 $(document).ready(function () {
-  $('.doneTestsList').append(`
-  <button type="button" class="list-group-item" id="elev">ELEVLISTA</button>
-  <button type="button" class="list-group-item" id="elev1">Ali</button>
-  <button type="button" class="list-group-item" id="elev2">Louise</button>
-  <button type="button" class="list-group-item" id="elev3">Dijana</button>
-  <button type="button" class="list-group-item" id="elev4">Masoud</button>
-  <button type="button" class="list-group-item" id="elev5">Henrik</button>
-  <button type="button" class="list-group-item" id="elev6">Magnus</button>
-`);
-
-$('.teachReadtest').append(`
-  <button type="button" class="list-group-item" id="doneTest">Färdiga prov</button>
-  <button type="button" class="list-group-item" id="doneTest1">ProvNamn: JAVA1</button>
-  <button type="button" class="list-group-item" id="doneTest2">ProvNamn: Java2</button>
-  <button type="button" class="list-group-item" id="doneTest3">ProvNamn: JavaScript</button>
-`);
-
-$('.displayDoneTest').append(`
- <h2>Färdigt prov</h2>
- <h4>Namn: </h4>
- <h4>Prov:</h4>
- <h4>Datum:</h4>
-`);
-});
-
-	//gömmer färdiga-prov-listan i början
-	$(document).ready(function(){
-		$('.teachReadtest').hide();
-	});
+	$('.teachReadtest').hide();
+	$('.displayDoneTest').hide();
 	
 	//vid click av elev dyker listan upp och vid click igen gömmer den sig
-	$(document).ready(function(){
-	$('#elev1').click(function(){
-		$('.teachReadtest').toggle();
-	});
-	$('#elev2').click(function(){
-		$('.teachReadtest').toggle();
-	});
-	$('#elev3').click(function(){
-		$('.teachReadtest').toggle();
-	});
-	$('#elev4').click(function(){
-		$('.teachReadtest').toggle();
-	});
-	$('#elev5').click(function(){
-		$('.teachReadtest').toggle();
-	});
-	$('#elev6').click(function(){
-		$('.teachReadtest').toggle();
-	});
-	});
-	
-	//gömmer färdigt prov
-	$(document).ready(function(){
-		$('.displayDoneTest').hide();
-	});
+  $('.btn-elev').on('click',function(){
+    $('.teachReadtest').toggle();
+  });
+
 	
 	//Vid click av färdigt prov
-	$(document).ready(function(){
-	$('#doneTest1').click(function(){
+	$('.btn-prov').click(function(){
 		$('.displayDoneTest').toggle();
 	});
-	$('#doneTest2').click(function(){
-		$('.displayDoneTest').toggle();
-	});
-	$('#doneTest3').click(function(){
-		$('.displayDoneTest').toggle();
-	});
-	});
-                 
+
+  
+});
--- med nya skelettet/js/insertStudentAndMailList.js
+++ med nya skelettet/js/insertStudentAndMailList.js
@@ -1,38 +1,3 @@
-$(document).ready(function () {
-  $('.generateMailButton').append(`
-  <button type="button" class="generateURL btn btn-default">Generera URL</button>
-`);
-
-  $('.copyMailListButton').append(`
-  <label class="MarkAllButton"><input type="checkbox" id="checkAll"/>Check all</label>
-`);
-
-$('.studClassesMailChoice').append(`
-  <button type="button" class="list-group-item" id="klass">KlassLista</button>
-  <button type="button" class="list-group-item" id="klass1">JAVA</button>
-  <button type="button" class="list-group-item" id="klass2">JavaScript</button>
-  <button type="button" class="list-group-item" id="klass3">JavaScript2</button>
-  <button type="button" class="list-group-item" id="klass4">HTML/CSS SnabbKurs</button>
-  <button type="button" class="list-group-item" id="klass5">JAVA2</button>
-  <button type="button" class="list-group-item" id="klass6">JAVA3</button>
-
-`);
-
-
-$('.tempMailAdressesList').append(`
-  
-  <li class="list-group-item"><label class="alternativ"><input type="checkbox" id="email"><span id="email-font">..@email.com</span></label></li>
-  <li class="list-group-item"><label class="alternativ"><input type="checkbox" id="email"><span id="email-font">...@email.com</span></label></li>
-  <li class="list-group-item"><label class="alternativ"><input type="checkbox" id="email"><span id="email-font">...@email.com</span></label></li>
-  <li class="list-group-item"><label class="alternativ"><input type="checkbox" id="email"><span id="email-font">..@email.com</span></label></li>
-  <li class="list-group-item"><label class="alternativ"><input type="checkbox" id="email"><span id="email-font">..@email.com</span></label></li>
-  <li class="list-group-item"><label class="alternativ"><input type="checkbox" id="email"><span id="email-font">..@email.com</span></label></li>
-  <li class="list-group-item"><label class="alternativ"><input type="checkbox" id="email"><span id="email-font">..@email.com</span></label></li>
-  <li class="list-group-item"><label class="alternativ"><input type="checkbox" id="email"><span id="email-font">..@email.com</span></label></li>
-  
-`);
-
-});
 	//PopUp fönster med LÄNK+ felmeddelande
 	function länkTillElevView() {
     var länk = prompt("Skriv in länk", "localhost:3000/about-us");
@@ -42,44 +7,24 @@ $('.tempMailAdressesList').append(`
     }
 	else{
 		document.getElementById("errorMessage").innerHTML =
-        "Länk: " + länk + " Ej funnen!";
+        "Länk: Ej funnen!";
 	}};
-	
+
+$(document).ready(function () {
 	//gömmer email-listan i början
-	$(document).ready(function(){
-		$('#lista').hide();
-	});
+	$('.tempMailAdressesList').hide();
 	
 	//vid click av klass dyker listan upp och vid click igen gömmer den sig
-	$(document).ready(function(){
-	$('#klass1').click(function(){
-		$('#lista').toggle();
+	$('.btn-klass').click(function(){
+		$('.tempMailAdressesList').toggle();
 	});
-	$('#klass2').click(function(){
-		$('#lista').toggle();
-	});
-	$('#klass3').click(function(){
-		$('#lista').toggle();
-	});
-	$('#klass4').click(function(){
-		$('#lista').toggle();
-	});
-	$('#klass5').click(function(){
-		$('#lista').toggle();
-	});
-	$('#klass6').click(function(){
-		$('#lista').toggle();
-	});
-	});
-	
-	$(document).ready(function(){
+
 	//Check all checkboxes - Markera alla emailadresser
 	$("#checkAll").change(function () {
 		$("input:checkbox").prop('checked', $(this).prop("checked"));
 	});
-	});
 
-
+});
 
 
 
