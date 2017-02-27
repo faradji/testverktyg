d0204559cc86f9fd333cbbf6d726d951e0f226e9
Dijana <dockan_7@hotmail.com>
2017-02-27 16:08:42 +0100

    changed lfunctions in teacherview

--- med nya skelettet/js/insertDoneTestsListInTeachView.js
+++ med nya skelettet/js/insertDoneTestsListInTeachView.js
@@ -3,15 +3,8 @@ $(document).ready(function () {
 	$('.displayDoneTest').hide();
 	
 	//vid click av elev dyker listan upp och vid click igen gömmer den sig
-  $('.btn-elev').on('click',function(){
-    $('.teachReadtest').toggle();
+  $('.btn-prov').on('click',function(){
+    $('.displayDoneTest').toggle();
   });
-
-	
-	//Vid click av färdigt prov
-	$('.btn-prov').click(function(){
-		$('.displayDoneTest').toggle();
-	});
-
-  
+ 
 });
