f7838a2eb1dad072531770c623a49494171ce2d8
Unknown <magnusnorlin@gmail.com>
2017-02-28 18:56:05 +0100

    teacherView, vet inte vad, tvingad att commita

--- js/classes/teacherview.class.js
+++ js/classes/teacherview.class.js
@@ -1,8 +1,30 @@
 class teacherView extends Base {
 
-  constructor(propertyValues){
-    super(propertyValues);
+  constructor(){
+    super();
+
+    new Survey(props, (survey)=>{
+      this.survey = survey;
+      new QuestionList((questions)=>{
+        this.questions = questions;
+        callback(this);
+      });
+    });
   }
-  
+
+/*   saveResponse(event){
+    event.preventDefault();
+    console.log('response saved');
+    console.log('this.questions', this.questions);
+    for(let q of this.questions){
+      let r = new Response({
+        question: q.id,
+        value: q.value,
+        survey: q.survey,
+        user: 3 // fake user
+      });
+      r.insert();
+    }
+  } */
 
 }
