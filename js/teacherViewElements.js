545a9f42576642c036f8c8c0fa408674b49512c7
Unknown <ali@faradji.se>
2017-03-01 13:29:55 +0100

    lagt till att prov visas

--- js/teacherViewElements.js
+++ js/teacherViewElements.js
@@ -1,10 +1,11 @@
 var divElement = $(document).find(".teacherElements"); 
+var divElementDoneTest = $(document).find(".doneTest");
 $(document).ready(function () {
 
 	//finds done tests and displays them under the name
-	for(var i = 0; i < window.userFromDb.length; i++)
+	for(let i = 0; i < window.userFromDb.length; i++)
 	{	
-		for(var j = 0; j < window.answersFromDb.length; j++)
+		for(let j = 0; j < window.answersFromDb.length; j++)
 		{
 			if(window.answersFromDb[j].Users_idUsers == window.userFromDb[i].idUsers ){
 				
@@ -17,9 +18,25 @@ $(document).ready(function () {
 			}	
 		}
   }
-					//	Users_idUsers":2,"Questions_idQuestions":1,"user_answer":1,
-						//"user_choice_no":"No","user_choice_yes":"Yes","score":"0/10"} 
-
+  
+  	//display chosen test
+   $(this).find(".linkToTest").on("click", function (event) {
+		event.preventDefault();
+	for(let i = 0; i < window.answersFromDb.length; i++)
+	{	
+		if(window.answersFromDb[i].user_answer==1){
+		$('<article>'+'question number: '+ window.answersFromDb[i].Questions_idQuestions+
+		' user answer: '+ 'Yes'+ 'total score was: '+window.answersFromDb[i].score +
+		'</article></br>').appendTo(divElementDoneTest);
+		}else{
+			$('<article>'+'question number: '+ window.answersFromDb[i].Questions_idQuestions+
+		' user answer: '+ 'No'+ 'total score was: '+window.answersFromDb[i].score +
+		'</article></br>').appendTo(divElementDoneTest);
+		}
+	}
+	
+   });
+					
 
 });
 
