9cf5cf72d2196394d858364211330be6d1dd0235
Unknown <magnusnorlin@gmail.com>
2017-03-06 14:13:55 +0100

    inget för er andra

--- js/mailViewElements.js
+++ js/mailViewElements.js
@@ -2,37 +2,43 @@
  	//gömmer email-listan i början
 	$('.mailLista').hide();
 	var listOfStudents = $(document).find(".mailLista");
-
-	for (let i = 0; i < window.klasser.length; i++) {
-      $('<option value="'+  window.klasser[i].class +'"class="listOfClasses">'
-	  + window.klasser[i].class+
-	  '</option>').appendTo(".dropDown");
-    }
-	
 	
 	//vid click av klass dyker listan upp och vid click igen gömmer den sig
-	$(document).find(".listOfClasses").on("click", function () {
-		// funkar inte på chrome
-		var contentPanelId = $(this).text();
+		
+	$('.btn-klass').click(function(event){
 		$(document).find(".mailLista").empty();
-		console.log(contentPanelId);
+		var contentPanelId = $(this).text();
 		
 	for(let i = 0; i < window.userFromDb.length; i++)
 		{	
 			
 		if(window.userFromDb[i].class == contentPanelId ){
-		
-	  $('<article href="#" class="list-group-item">'+ window.userFromDb[i].emailAddress+
+			
+	  $('<article href="" class="list-group-item" >'+ window.userFromDb[i].emailAddress+
 	  '</article>').appendTo(listOfStudents);
+		
+		
+		
 		}
+			
 	  }
 		
 		$('.mailLista').show();
 	});
-
-$("#sendLinkButton").click(function() {
-
-    var länk = prompt("Skriv in länk", "localhost:3000/elev");
-
+		//Check all checkboxes - Markera alla emailadresser
+	// $("#checkAll").change(function () {
+		// $("input:checkbox").prop('checked', $(this).prop("checked"));
+	// }); 
 });
-});
+
+function länkTillElevView() {
+    var länk = prompt("Kopiera länken", "localhost:3000/elev");
+    // if (länk != null) {
+        // document.getElementById("errorMessage").innerHTML =
+        // "Länk: " + länk + " Skickad!";
+    // }
+	// else{
+		// document.getElementById("errorMessage").innerHTML =
+        // "Länk: Ej funnen!";
+	// }
+};
